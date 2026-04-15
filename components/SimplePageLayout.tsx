73702a3e7afb6fc0ab53f8f8c1545a3eb9e39921
rogue-studio <soyevermail@gmail.com>
2026-04-16 03:32:29 +0900

    SimplePageLayout.tsx を更新

--- components/SimplePageLayout.tsx
+++ components/SimplePageLayout.tsx
@@ -6,7 +6,7 @@ type Props = {
 export default function SimplePageLayout({ title, children }: Props) {
   return (
     <main className="min-h-screen bg-[#f3efe7] text-[#1a1a1a] px-6">
-      <div className="mx-auto max-w-[520px] py-40 pl-2">
+      <div className="mx-auto max-w-[520px] pb-20 pt-28">
         <h1 className="mb-4 text-[28px] font-normal uppercase tracking-[0.14em] text-[#161616]/88">
           {title}
         </h1>
