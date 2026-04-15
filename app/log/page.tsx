caf792eb53aa73783bfdada67f238e66a8125ccb
rogue-studio <soyevermail@gmail.com>
2026-04-16 02:36:49 +0900

    page.tsx を更新

--- app/log/page.tsx
+++ app/log/page.tsx
@@ -12,9 +12,13 @@ export default function LogPage() {
         LOG
       </h1>
 
-      <div className="text-[15px] leading-[2.1] text-[#161616]/68">
-        under consideration
-      </div>
+    <div className="text-[15px] leading-[2.1] text-[#161616]/68">
+  <p>「プロジェクト準備中のルーティン」</p>
+  <p>時間の余白の尊さを綴りながら、現実は深夜作業。</p>
+  <p>朝方に繰り出す大欠伸は私の矛盾の集大成か。</p>
+  <p>でも、誰かに何かを届けたくて、言葉に悩む時間て。</p>
+  <p>本末転倒するくらいじゃないと面白くないなって確信がある。</p>
+</div>
     </main>
   );
 }
